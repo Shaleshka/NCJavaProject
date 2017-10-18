5d83d73e7fca38fe65868ce8ef5685feac6d6641
SHALESHKA-ПК\Shaleshka <bshaleshka@gmail.com>
2017-10-18 13:16:07 +0300

    Added first entity

---
+++ backend/src/main/java/com/netcracker/devschool/dev4/studdist/entity/User.java
@@ -0,0 +1,75 @@
+package com.netcracker.devschool.dev4.studdist.entity;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by Shaleshka on 16.10.17.
+ */
+@Entity
+public class User {
+
+    private long id;
+    private String email;
+    private String password;
+    private int role;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+        return email.equals(user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + email.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                '}';
+    }
+
+
+}
